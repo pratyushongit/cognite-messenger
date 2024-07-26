0246e80d374a00d6b8585d45a7f215054e5946de
Pratyush Biswas <pratyush.biswas1@gmail.com>
2024-07-26 11:55:37 +0530

    Add assets required for the project

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import avatar from "./images/avatar.png";
+import search from "./images/search.png";
+import add from "./images/add.png";
+import minus from "./images/minus.png";
+import logo from "./images/logo.png";
+
+export const IMAGES = {
+  avatar,
+  search,
+  add,
+  minus,
+  logo,
+};
